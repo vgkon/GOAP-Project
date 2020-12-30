03784ed631fe8b944a595bda468acfef95c3b545
vgkon <vgkonstantakopoulos@gmail.com>
2020-12-30 19:03:01 +0200

    Revert "WInterface"
    This reverts commit 2377b5caee6849e4cd4ce215c668683cfc4a1d1d.

--- Assets/Scripts/GOAP/GWorld.cs
+++ Assets/Scripts/GOAP/GWorld.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 
 public class ResourceQueue
@@ -41,11 +40,6 @@ public class ResourceQueue
         if (que.Count == 0) return null;
         GWorld.Instance.GetWorld().ModifyState(modState, -1);
         return que.Dequeue();
-    }
-    public void RemoveResource(GameObject r)
-    {
-        que = new Queue<GameObject>(que.Where(p => p != r));
-        GWorld.Instance.GetWorld().ModifyState(modState, -1);
     }
 }
 
