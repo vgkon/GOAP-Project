d896782692b95a7ff705fb1acae2b389c8b30a1c
vgkon <vgkonstantakopoulos@gmail.com>
2020-12-30 19:02:09 +0200

    Revert "included pause"
    This reverts commit db2f3d54c5bc783c54f2c023d52eb16bd7d6eba9.

--- Assets/Scripts/Game/UpdateWorld.cs
+++ Assets/Scripts/Game/UpdateWorld.cs
@@ -30,10 +30,5 @@ public class UpdateWorld : MonoBehaviour
         {
             Application.Quit();
         }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Time.timeScale = (Time.timeScale == 0) ? 1 : 0;
-        }
     }
 }
